c119360a3f02835837768b1e355887aa859c719d
Joao Malheiro de Sousa <jonas_malheiro_sousa@hotmail.com>
2017-11-13 23:09:14 +0000

    Halfway done app.h documented

--- Application.h
+++ Application.h
@@ -15,37 +15,140 @@ using namespace std;
 class Application {
 
 private:
+    /** @name Application data-members*/
+    /** @{
+    *
+    */
+
+    /**
+     * @brief Company company
+     */
     Company company;
+    /**
+     * @brief bool passengersChanged is true when Passengers vector has changed and false otherwise
+     */
     bool passengersChanged;
+    /**
+     * @brief bool airplanesChanged is true when Airplanes vector has changed and false otherwise
+     */
     bool airplanesChanged;
+    /**
+     * @brief bool flightsChanged is true when Flights vector has changed and false otherwise
+     */
     bool flightsChanged;
+    /**
+     * @brief string passengersFilepath is the name of the file containing the Passengers information
+     */
     string passengersFilepath;
-    string airplanesFilepath;
+    /**
+    * @brief string airplanesFilepath is the name of the file containing the Airplanes information
+    */
+    string airplanesFilepath
+    /**
+    * @brief string flightsFilepath is the name of the file containing the Flights information
+    */
     string flightsFilepath;
+    /** @} end of Application data-members */
 
 public:
+    /**
+     * @brief Constructor of an "empty" Application object
+     */
     Application();
 
-    //menus
+    /** @name Application Menu member-functions*/
+    /** @{
+    *
+    */
+
+    /**
+     * @brief Main menu with the following options: File, Passenger, Airplane management, Booking and quit
+     */
     void mainMenu();
+    /**
+     * @brief Menu that allows the user to manage the files
+     */
     void filesMenu();
+    /**
+     * @brief Menu to manage Passengers
+     */
     void passengersMenu();
+    /**
+     * @brief Menu to manage Airplanes
+     */
     void airplanesMenu();
+    /**
+     * @brief Menu to manage the Flights of a certain Airplane passed as parameter with a pointer
+     * @param *airplane Airplane *airplane
+     */
     void flightsMenu(Airplane *airplane);
+    /**
+     * @brief Menu to manage bookings
+     */
     void bookingsMenu();
 
-    //passenger fucntions
+    /** @} end of Application Menu member-functions */
+
+//passenger functions
+/** @name Application using Passengers member-functions*/
+    /** @{
+    *
+    */
+
+    /**
+    * @brief Prints the total information of a desired Passenger
+    */
     void passengerShow();
+    /**
+     * @brief Creates a new Passenger and adds it to the Company vector passengers after validating the information
+     */
     void passengerCreate();
+    /**
+     * @brief Deletes the pretended Passenger from the Company vector containing the passengers
+     */
     void passengerDelete();
+    /**
+     * @brief Selects a menu that allows the user to Update information relative to a Passenger (name, date of birth, job and average number of flights per year)
+     */
     void passengerUpdateMenu();
+    /**
+     * @brief Prints a summary of a Passenger object
+     */
     void printSummaryPassenger();
+    /**
+     * @brief Searches the passengers vector for the id inserted by the user and returns the pretended Passenger
+     * @return Passenger*
+     */
     Passenger * choosePassenger();
+    /**
+     * @brief Checks if the id is in the vector of passengers, if it is an exception is thrown
+     * @param id int id
+     */
     void validPassenger(int id);
+    /**
+     * @brief Updates the name of the passenger passed as parameter to the one inserted by the user
+     * @param *passenger Passenger *passenger
+     */
     void passengerUpdateName(Passenger * passenger);
+    /**
+     * @brief Updates the date of birth of the passenger passed as parameter to the one inserted by the user
+     * @param *passenger Passenger *passenger
+     */
     void passengerUpdateDateOfBirth(Passenger * passenger);
+    /**
+     * @brief Updates the job of the passenger passed as parameter to the one inserted by the user
+     * @param *passenger Passenger *passenger
+     */
     void passengerUpdateJob(Passenger * passenger);
+    /**
+     * @brief Updates the average number of flights per year of the passenger passed as parameter to the one inserted by the user
+     * @param *passenger Passenger *passenger
+     */
     void passengerUpdateNYear(Passenger * passenger);
+
+    /** @} end of Application using Passengers member-functions */
+
+    
 
     //airplane functions
     void airplaneShow();
