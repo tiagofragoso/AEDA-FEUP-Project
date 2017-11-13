91b543a3ad12e56b7fab34fdba19fd938c365730
Mariana <marianabarbosaaguiar@gmail.com>
2017-11-13 21:55:02 +0000

    cenas

--- Application.cpp
+++ Application.cpp
@@ -1046,7 +1046,7 @@ void Application::airplaneUpdateCapacity(Airplane *airplane) {
 
 }
 
-void Application::flightUpdatePrice(Airplane airplane, Flight * flight)
+void Application::flightUpdatePrice(Airplane *airplane, Flight * flight)
 {
 	int newPrice;
 	cout << "The current price for the chosen flight is '" << flight->getBasePrice() << "'.\n";
@@ -1058,7 +1058,7 @@ void Application::flightUpdatePrice(Airplane airplane, Flight * flight)
 	} while (true);
 
 	flight->setBasePrice(newPrice);
-	for (size_t i = 0; i < airplane.getFlights().size(); i++)
+	for (size_t i = 0; i < airplanegetFlights().size(); i++)
 	{
 		if (airplane.getFlights().at(i) == flight)
 		{
