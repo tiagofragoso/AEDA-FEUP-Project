65c9a439296ec1aa70b5bbb9028802d2c39e1fa1
Tiago Fragoso <tiagofragoso@outlook.com>
2017-11-21 10:49:32 +0000

    template binary

--- helper.h
+++ helper.h
@@ -14,8 +14,6 @@ using namespace std;
  */
 
 
-
-
 enum type {
     PID = 1,
     PNAME = 2,
@@ -28,38 +26,57 @@ enum type {
     FTIME = 9
 };
 
-struct Date{
+struct Date {
     unsigned int day = 0;
     unsigned int month = 0;
     unsigned int year = 0;
 
     string print();
+
     Date();
+
     Date(string &s);
 };
+
 /** @} end of Date Struct */
 
-template <class T> struct less : binary_function <T,T,bool> {
-    bool operator() (const T& x, const T& y) const {
-        if( x.length() < y.length()) {
+template<class T>
+struct less : binary_function<T, T, bool> {
+    bool operator()(const T &x, const T &y) const {
+        if (x.length() < y.length()) {
             return true;
         } else if (x.length() > y.length()) {
             return false;
-        }else return (x < y);
+        } else return (x < y);
     }
 };
 
 
-
 bool validArg(int &variable);
+
 bool validArg(int &variable, bool &end);
+
 void trimString(string &s);
+
 void pause();
+
 void normalize(string &s);
+
 void next(int &elem, string &piece, string separator);
+
 void next(string &piece, string &line, string separator);
 
-
+template<class T>
+T binary_search_ptr(const vector<T> &v, T x, bool comp(T t1, T t2)) {
+    int left = 0, right = v.size() - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (comp(v[middle], x)) left = middle + 1;
+        else if (comp(x, v[middle])) right = middle - 1;
+        else return v[middle];
+    }
+    return nullptr;
+}
 
 
 #endif /* SRC_HELPER_H_ */
