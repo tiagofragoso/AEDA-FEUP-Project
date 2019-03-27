952a1d538f06bf812d5c7d27a2074310b16490f0
Júlio Santos <hi@whoisjuliosantos.com>
2019-03-27 16:22:35 +0000

    Fix whitespace

--- Booking.cpp
+++ Booking.cpp
@@ -1,22 +1,23 @@
 #include "Booking.h"
 
-Booking::Booking(unsigned int id, Passenger *passenger, Flight *flight, string seat) : passenger(passenger),
-                                                                                       flight(flight), seat(seat),
-                                                                                       id(id) {}
-
+Booking::Booking(unsigned int id, Passenger *passenger, Flight *flight, string seat)
+  : passenger(passenger),
+    flight(flight),
+    seat(seat),
+    id(id) {}
 
 Flight *Booking::getFlight() {
-    return this->flight;
+  return this->flight;
 }
 
 string Booking::getSeat() {
-    return this->seat;
+  return this->seat;
 }
 
 Passenger *Booking::getPassenger() {
-    return this->passenger;
+  return this->passenger;
 }
 
 unsigned int Booking::getId() {
-    return this->id;
+  return this->id;
 }
